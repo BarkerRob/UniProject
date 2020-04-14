a7e8adeafa39fa381ec7c3ffc9466c3400cb5702
Rob Blagg <blaggrob@gmail.com>
2020-04-14 08:58:16 +0100

    Started investigation for implemention random forest algorithm

---
+++ python/random_forest_algorithm.py
@@ -0,0 +1,11 @@
+"""
+Notes:
+    I need to convert the win/draw/loss into a value between 0 and 1.
+    Initial thoughts are below:
+     A draw is 0.5
+     A win by 1 goal is 0.4
+     A win by 2 goals is 0.3
+     A win by 3 goals is 0.2
+     A win by 4 goals is 0.1
+     A win by 5+ goals is 0.05
+"""
