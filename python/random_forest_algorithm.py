258a17bc0cc3fa0cf725f8e5fe4daf2bd88eb225
Rob Blagg <blaggrob@gmail.com>
2020-04-26 15:34:55 +0100

    Fixed recent form to include away teams form too

--- python/random_forest_algorithm.py
+++ python/random_forest_algorithm.py
@@ -48,13 +48,14 @@ def main():
         database='plepa'
     )
     db_cursor = plepa_db.cursor()
+    team_one = 1
+    team_two = 2
 
-    # TODO need to remove the 1 passing into this function, it needs to be passed in dynamically
-    recent_form(1, db_cursor)
-    form_against_team(1, 2, db_cursor)
-    distance_travelled(1, 2, db_cursor)
-    league_difference(1, 2, db_cursor)
-    current_league_difference(14, 19, db_cursor)
+    recent_form(team_one, team_two, db_cursor)
+    form_against_team(team_one, team_two, db_cursor)
+    distance_travelled(team_one, team_two, db_cursor)
+    league_difference(team_one, team_two, db_cursor)
+    current_league_difference(team_one, team_two, db_cursor)
 
     plepa_db.close()
 
@@ -62,23 +63,41 @@ def main():
 # ===================================================== Functions ==================================================== #
 
 
-def recent_form(team_id, db_cursor):
+def recent_form(team_id_one, team_id_two,  db_cursor):
+    # TODO Maybe this should compare the two teams forms
     sql = f'SELECT * ' \
           f'FROM plepa.game ' \
-          f'WHERE home_team_id_pk_fk = {team_id}  OR away_team_id_pk_fk = {team_id} ' \
+          f'WHERE home_team_id_pk_fk = {team_id_one}  OR away_team_id_pk_fk = {team_id_one} ' \
           f'ORDER BY game_pk DESC ' \
           f'LIMIT 5'
     db_cursor.execute(sql)
-    form = 0
+    team_one_form = 0
     for game_nr, home_team_id, away_team_id, season, home_team_score, away_team_score in db_cursor:
-        if home_team_id == team_id:
+        if home_team_id == team_id_one:
             result = home_team_score - away_team_score
         else:
             result = away_team_score - home_team_score
         if result > 0:
-            form = form + 0.2
+            team_one_form = team_one_form + 0.2
         elif result == 0:
-            form = form + 0.1
+            team_one_form = team_one_form + 0.1
+    sql = f'SELECT * ' \
+          f'FROM plepa.game ' \
+          f'WHERE home_team_id_pk_fk = {team_id_two}  OR away_team_id_pk_fk = {team_id_two} ' \
+          f'ORDER BY game_pk DESC ' \
+          f'LIMIT 5'
+    db_cursor.execute(sql)
+    team_two_form = 0
+    for game_nr, home_team_id, away_team_id, season, home_team_score, away_team_score in db_cursor:
+        if home_team_id == team_id_two:
+            result = home_team_score - away_team_score
+        else:
+            result = away_team_score - home_team_score
+        if result > 0:
+            team_two_form = team_two_form + 0.2
+        elif result == 0:
+            team_two_form = team_two_form + 0.1
+    form = team_one_form - (team_two_form / 3)
     print(form)
 
 
