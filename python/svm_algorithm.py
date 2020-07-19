6835de287ac1a440e5cf5cdbfd825f796433df17
Rob Blagg <blaggrob@gmail.com>
2020-07-19 20:11:47 +0100

    More work on SVM

--- python/svm_algorithm.py
+++ python/svm_algorithm.py
@@ -2,10 +2,12 @@
 """
 # ====================================================== Imports ===================================================== #
 
+from sklearn import svm
+import mysql.connector
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import svm, datasets
-import mysql.connector
+from matplotlib import style
+style.use("ggplot")
 
 # ===================================================== Variables ==================================================== #
 
@@ -21,13 +23,33 @@ def main():
         database='plepa'
     )
     db_cursor = plepa_db.cursor(buffered=True)
-    iris = datasets.load_iris()
     training_dataset = []
+    training_to_plot = []
     sql = f'SELECT * ' \
           f'FROM svm_data '
     db_cursor.execute(sql)
     for away_team_distance_travelled, league_difference, result in db_cursor:
         training_dataset.append([away_team_distance_travelled, league_difference, result])
+        training_to_plot.append([away_team_distance_travelled, league_difference])
+    result_plot = []
+    training_plot = np.array(training_to_plot)
+    for result in training_dataset:
+        if result[2] == 'WIN':
+            result_plot.append(1)
+        else:
+            result_plot.append(0)
+    clf = svm.SVC(kernel='linear', C=1.0)
+    clf.fit(training_plot, result_plot)
+    w = clf.coef_[0]
+    print(w)
+    a = -w[0] / w[1]
+    xx = np.linspace(0, 12)
+    yy = a * xx - clf.intercept_[0] / w[1]
+    h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+    plt.scatter(training_plot[:, 0], training_plot[:, 1], c=result_plot)
+    plt.legend()
+    plt.show()
+
     test_dataset = []
     sql = f'SELECT * ' \
           f'FROM svm_test_data '
